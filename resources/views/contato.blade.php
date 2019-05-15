77a5833fd402777b04bd4f8e78fdfc235fbbf1ae
Gabriel Caetano <gabriel.goulartcaetano@gmail.com>
2019-05-14 23:52:50 -0300

    Informações incorretas no Footer.

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -144,52 +144,7 @@
                 <div class="col-lg-12 footer_col">
                     <div class="footer_links">
                         <div class="row">
-                            <div class="col-sm-6 footer_list_col magic_fade_in">
-                                <div class="footer_list_container">
-                                    <div class="footer_list_title">Hosting Options</div>
-                                    <ul class="footer_list">
-                                        <li><a href="#">Web Hosting</a></li>
-                                        <li><a href="#">WordPress Hosting</a></li>
-                                        <li><a href="#">VPS Hosting</a></li>
-                                        <li><a href="#">Cloud Server</a></li>
-                                        <li><a href="#">Reseller Package</a></li>
-                                        <li><a href="#">Dedicated Hosting</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-sm-6 footer_list_col magic_fade_in">
-                                <div class="footer_list_container">
-                                    <div class="footer_list_title">Domains</div>
-                                    <ul class="footer_list">
-                                        <li><a href="#">Buy a Domain</a></li>
-                                        <li><a href="#">Premium Domain Names</a></li>
-                                        <li><a href="#">Web Hosting</a></li>
-                                        <li><a href="#">Transfer Your Domain</a></li>
-                                        <li><a href="#">Domain Marketplace</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-sm-6 footer_list_col magic_fade_in">
-                                <div class="footer_list_container">
-                                    <div class="footer_list_title">Resellers</div>
-                                    <ul class="footer_list">
-                                        <li><a href="#">VPS Hosting</a></li>
-                                        <li><a href="#">Cloud Server</a></li>
-                                        <li><a href="#">Reseller Package</a></li>
-                                        <li><a href="#">Dedicated Hosting</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-sm-6 footer_list_col magic_fade_in">
-                                <div class="footer_list_container">
-                                    <div class="footer_list_title">Support</div>
-                                    <ul class="footer_list">
-                                        <li><a href="#">Buy a Domain</a></li>
-                                        <li><a href="#">Premium Domain Names</a></li>
-                                        <li><a href="#">Web Hosting</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+
                         </div>
                     </div>
                 </div>
